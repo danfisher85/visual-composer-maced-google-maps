b728166038a5990caa850af2cde953f96608fda3
Dan Fisher <dan.fisher85@yahoo.com>
2016-07-05 12:15:00 +0200

    Spaces changes

--- visual-composer-maced-google-maps.php
+++ visual-composer-maced-google-maps.php
@@ -274,11 +274,11 @@ class vcMacedGmap
             $output .= 'var latlng = new google.maps.LatLng(' . $lat . ',' . $lng . ');';
             
             $output .= 'var myOptions = {';
-            $output .= 'zoom				: ' . intval($zoom) . ',';
-            $output .= 'center				: latlng,';
-            $output .= 'mapTypeId			: google.maps.MapTypeId.ROADMAP,';
+            $output .= 'zoom                : ' . intval($zoom) . ',';
+            $output .= 'center              : latlng,';
+            $output .= 'mapTypeId           : google.maps.MapTypeId.ROADMAP,';
             if ( $styles == 'ultra_light_with_labels') {
-                $output .= 'styles	: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}],';
+                $output .= 'styles  : [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}],';
             } elseif ( $styles == 'unsaturated_browns') {
                 $output .= 'styles  : [{"elementType":"geometry","stylers":[{"hue":"#ff4400"},{"saturation":-68},{"lightness":-4},{"gamma":0.72}]},{"featureType":"road","elementType":"labels.icon"},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#0077ff"},{"gamma":3.1}]},{"featureType":"water","stylers":[{"hue":"#00ccff"},{"gamma":0.44},{"saturation":-33}]},{"featureType":"poi.park","stylers":[{"hue":"#44ff00"},{"saturation":-23}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"hue":"#007fff"},{"gamma":0.77},{"saturation":65},{"lightness":99}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"gamma":0.11},{"weight":5.6},{"saturation":99},{"hue":"#0091ff"},{"lightness":-86}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"lightness":-48},{"hue":"#ff5e00"},{"gamma":1.2},{"saturation":-23}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"saturation":-64},{"hue":"#ff9100"},{"lightness":16},{"gamma":0.47},{"weight":2.7}]}],';
 
@@ -316,19 +316,19 @@ class vcMacedGmap
                 $output .= 'styles  : [{"featureType":"administrative","stylers":[{"visibility":"off"}]},{"featureType":"poi","stylers":[{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"water","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","stylers":[{"visibility":"simplified"}]},{"featureType":"landscape","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"visibility":"off"}]},{"featureType":"road.local","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"water","stylers":[{"color":"#84afa3"},{"lightness":52}]},{"stylers":[{"saturation":-17},{"gamma":0.36}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"color":"#3f518c"}]}],';
 
             }
-            $output .= 'zoomControl			: ' . $zoomControl . ',';
-            $output .= 'mapTypeControl		: ' . $mapTypeControl . ',';
-            $output .= 'streetViewControl	: ' . $streetViewControl . ',';
-            $output .= 'scrollwheel			: false';
+            $output .= 'zoomControl         : ' . $zoomControl . ',';
+            $output .= 'mapTypeControl      : ' . $mapTypeControl . ',';
+            $output .= 'streetViewControl   : ' . $streetViewControl . ',';
+            $output .= 'scrollwheel         : false';
             $output .= '};';
             
             $output .= 'var map = new google.maps.Map(document.getElementById("google-map-area-' . $uid . '"), myOptions);';
             
             $output .= 'var marker = new google.maps.Marker({';
-            $output .= 'position			: latlng,';
+            $output .= 'position            : latlng,';
             if ($icn)
-                $output .= 'icon	: "' . $icn . '",';
-            $output .= 'map					: map';
+                $output .= 'icon    : "' . $icn . '",';
+            $output .= 'map                 : map';
             $output .= '});';
             
             // additional markers
@@ -349,10 +349,10 @@ class vcMacedGmap
                     $markerID = 'marker' . $markerID;
                     
                     $output .= 'var ' . $markerID . ' = new google.maps.Marker({';
-                    $output .= 'position			: new google.maps.LatLng(' . $v . '),';
+                    $output .= 'position            : new google.maps.LatLng(' . $v . '),';
                     if ($icn)
-                        $output .= 'icon	: "' . $icn . '",';
-                    $output .= 'map					: map';
+                        $output .= 'icon    : "' . $icn . '",';
+                    $output .= 'map                 : map';
                     $output .= '});';
 
                     $output .= 'bounds.extend(new google.maps.LatLng(' . $v . '));';
